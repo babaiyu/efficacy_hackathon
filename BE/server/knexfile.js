8260b2e6d4ee0dcee02bf05496acdf444d8ce7ff
Ravi Deevan <ravideevan3@gmail.com>
2020-10-12 04:49:45 +0800

    Added production knex

--- BE/server/knexfile.js
+++ BE/server/knexfile.js
@@ -16,4 +16,18 @@ module.exports = {
 			directory: './db/seeds',
 		},
 	},
+	production: {
+		client: 'pg',
+		connection: {
+			database: process.env.DB_DATABASE,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+		},
+		migrations: {
+			directory: './db/migrations',
+		},
+		seeds: {
+			directory: './db/seeds',
+		},
+	},
 };
