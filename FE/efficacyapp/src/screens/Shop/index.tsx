3c9eb142476b413d8b4158a0f69270b4f06b086e
babaiyu <bayuputra9a@gmail.com>
2020-10-12 00:52:59 +0700

    Designing Shop

--- FE/efficacyapp/src/screens/Shop/index.tsx
+++ FE/efficacyapp/src/screens/Shop/index.tsx
@@ -1,11 +1,19 @@
+import {myColors} from 'constants/colors';
+import {fonts} from 'constants/fonts';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {StyleSheet, View, TextInput} from 'react-native';
+import ListItem from './ListItem';
 
 function Shop() {
+  // State
+  const [data, setData] = React.useState([1, 2, 3, 4, 5]);
+
   return (
     <View style={styles.container}>
-      <Text>Shop</Text>
+      <View style={styles.content}>
+        <TextInput style={styles.formInput} placeholder="Search..." />
+        <ListItem data={data} />
+      </View>
     </View>
   );
 }
@@ -15,7 +23,22 @@ export default Shop;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
+  },
+  content: {
+    width: '90%',
+    marginTop: 20,
+  },
+  formInput: {
+    borderRadius: 25,
+    backgroundColor: myColors.white,
+    color: myColors.dark,
+    textDecorationColor: myColors.dark,
+    fontFamily: fonts.medium,
+    textAlign: 'left',
+    paddingLeft: 20,
+    marginVertical: 5,
+    borderWidth: 1,
+    borderColor: myColors.white,
   },
 });
